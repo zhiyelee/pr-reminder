166f392144c23606654d3103d3a4350a6ba0e744
zhiyelee <zhiyelee@gmail.com>
2019-01-09 01:29:05 -0600

    fix link

--- helpers/gh_events.py
+++ helpers/gh_events.py
@@ -18,10 +18,14 @@ def review_requested_handler(event):
 
     # requested or removed reviewer
     reviewer = User(event['requested_reviewer']['login'])
+
+    # we cannot send notification when there is no slack_id to notify
     if reviewer.slack_id is None:
         return
 
-    sender = event['sender']['login']
+    sender_user = User(event['sender']['login'])
+    # if slack_id is not available, use github_id instead
+    sender_slack_id = sender_user.github_id if sender_user.slack_id is None else sender_user.slack_id
 
     # pull request meta data
     pull_number = pull_request['number']
@@ -31,9 +35,9 @@ def review_requested_handler(event):
     pull_str = '<{2}|#{0}: {1}>'.format(pull_number, pull_title, pull_url)
 
     if event_action == 'review_requested':
-        reminder_message = '{0} requested you to CR {1}'.format(sender, pull_str)
+        reminder_message = '<@{0}> requested you to review {1}'.format(sender_slack_id, pull_str)
     elif event_action == 'review_request_removed':
-        reminder_message = 'You are removed as a reviewer for PR {0}'.format(pull_str)
+        reminder_message = 'You are removed as a reviewer for pull request {0}'.format(pull_str)
 
     send_to_slack_user(reminder_message, '@' + reviewer.slack_id)
     logger.info('remind message %s', reminder_message)
